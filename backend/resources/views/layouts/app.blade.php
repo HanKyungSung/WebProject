b69f5e67e16490c2667457bca459e691a317652f
HanKyungSung <gksruddlakstp@gmail.com>
2019-11-24 00:38:35 -0600

    Add dropdown item for settings and My posts. Also name shwos on drop down menu. Resolved #16

--- backend/resources/views/layouts/app.blade.php
+++ backend/resources/views/layouts/app.blade.php
@@ -48,15 +48,14 @@
                     <li class="nav-item">
                         <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                     </li>
-                    @if (Route::has('register'))
                     <li class="nav-item">
                         <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                     </li>
-                    @endif
-                    @else
+                    @endguest
+                    @auth
                     <li class="nav-item dropdown">
                         <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ Auth::user()->name }} <span class="caret"></span>
+                            {{ Auth::user()->first_name }} <span class="caret"></span>
                         </a>
 
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
@@ -65,12 +64,20 @@
                                 {{ __('Logout') }}
                             </a>
 
+                            <a class="dropdown-item" href="/user/{{ Auth::user()->id }}/settings" onclick="event.preventDefault();">
+                                Settings
+                            </a>
+
+                            <a class="dropdown-item" href="/user/{{ Auth::user()->id }}/posts" onclick="event.preventDefault();">
+                                My Posts
+                            </a>
+
                             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                 @csrf
                             </form>
                         </div>
                     </li>
-                    @endguest
+                    @endAuth
                 </ul>
             </div>
         </div>
