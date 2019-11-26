961f2084b509d4f0402775191b1dbd275f5b1fb6
HanKyungSung <gksruddlakstp@gmail.com>
2019-11-25 21:21:23 -0600

    Fix blade to show both auth/guest user. ~bug

--- backend/resources/views/post.blade.php
+++ backend/resources/views/post.blade.php
@@ -1,12 +1,14 @@
 @extends('layouts.app') @section('content')
 <div class="container">
-    @if(Auth::user()->id == $post->user->id)
-    <div class="row mb-2">
-        <div class="d-flex col-12 justify-content-end">
-            <button type="button" class="btn btn-success">Edit</button>
+    @auth
+        @if(Auth::user()->id == $post->user->id)
+        <div class="row mb-2">
+            <div class="d-flex col-12 justify-content-end">
+                <button type="button" class="btn btn-success">Edit</button>
+            </div>
         </div>
-    </div>
-    @endif
+        @endif
+    @endAuth
     <div class="row">
         <div class="col-12">
             <div class="card border-secondary mb-3">
@@ -17,11 +19,13 @@
                 </div>
             </div>
         </div>
-        @if(Auth::user()->id == $post->user->id)
-        <div class="d-flex col-12 justify-content-end">
-            <button type="button" class="btn btn-danger">Delete</button>
-        </div>
-        @endif
+        @auth
+            @if(Auth::user()->id == $post->user->id)
+            <div class="d-flex col-12 justify-content-end">
+                <button type="button" class="btn btn-danger">Delete</button>
+            </div>
+            @endif
+        @endAuth
         @foreach($comments as $comment)     
         <div class="col-12">
             <div class="card border-secondary mb-3">
